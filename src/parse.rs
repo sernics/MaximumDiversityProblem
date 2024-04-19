d56609ad1ca6ac4e462c6c0453c1b248c3ac5745
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-04-19 20:00:47 +0100

    Tab changes

--- src/parse.rs
+++ src/parse.rs
@@ -10,9 +10,10 @@ pub fn parse_file(path: &PathBuf) -> Vec<PointType> {
   let atributes_size = contents.lines().nth(1).unwrap().parse::<i32>().unwrap();
   for line in contents.lines().skip(2).take(points_size as usize) {
     let coords: Vec<f32> = line.split_whitespace()
-        .map(|s| s.replace(",", ".")
+      .map(|s| 
+        s.replace(",", ".")
         .parse::<f32>().unwrap())
-        .collect();
+      .collect();
     match atributes_size {
       2 => points.push(PointType::Point2d(Point2d::new(coords))),
       3 => points.push(PointType::Point3d(Point3d::new(coords))),
