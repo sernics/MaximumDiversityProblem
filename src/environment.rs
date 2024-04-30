66dbad6ad4e557dfd6aaea709f10ec33629b504b
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-04-30 12:18:20 +0100

    Solved tabu

--- src/environment.rs
+++ src/environment.rs
@@ -26,4 +26,23 @@ impl Environment {
     }
     return self.solution.clone();
   }
+  pub fn generate_neighbours(&mut self) -> Vec<MDPSolution> {
+    let mut neighbours: Vec<MDPSolution> = vec![];
+    for i in 0..self.solution.len() {
+      for j in 0..self.mdp_problem.states().len() as usize {
+        let actual_diversity = self.solution.calculate_diversity();
+        let drop_point = self.solution[i].clone();
+        self.solution.drop(i);
+        self.solution.insert_at(self.mdp_problem.states()[j].clone(), i);
+        let new_diversity = self.solution.calculate_diversity();
+        if new_diversity < actual_diversity {
+          self.solution.drop(self.solution.len() - 1);
+          self.solution.insert_at(drop_point, i);
+        } else {
+          neighbours.push(self.solution.clone());
+        }
+      }
+    }
+    return neighbours;
+  }
 }
--- src/mdp_tabu.rs
+++ src/mdp_tabu.rs
@@ -1,41 +1,53 @@
 use crate::mdp_solution::MDPSolution;
 use crate::mdp::MDP;
 use crate::mdp_problem::MDPProblem;
+use crate::mdp_grasp::MDPGrasp;
+use crate::environment::Environment;
+
+use std::collections::VecDeque;
 
 pub struct MDPTabu {
   mdp_solution: MDPSolution,
   size_m: u8,
-  tabu_list: Vec<MDPSolution>
+  tabu_list: VecDeque<MDPSolution>,
+  max_iterations: u8
 }
 
 impl MDP for MDPTabu {  
   fn new(problem: MDPProblem, size_m: u8) -> Self {
-    MDPTabu { mdp_solution: MDPSolution::new(problem), size_m, tabu_list: vec![] }
+    MDPTabu { mdp_solution: MDPSolution::new(problem), size_m, tabu_list: VecDeque::new(), max_iterations: 100 }
   }
   fn execute(&mut self) -> &MDPSolution {
-    self.mdp_solution.insert(self.mdp_solution.mdp_problem().initial_point().clone());
-    while self.mdp_solution.len() < self.size_m as usize {
-      let centroid = self.mdp_solution.centroids();
-      let selected = self.mdp_solution.mdp_problem().next_point(&centroid);
-      if !self.mdp_solution.contains(&selected) {
-        self.mdp_solution.insert(selected.clone());
-      } else {
-        let mut max_diversity = 0.0;
-        let mut selected = self.mdp_solution.mdp_problem().initial_point().clone();
-        for point in self.mdp_solution.mdp_problem().states() {
-          if !self.mdp_solution.contains(point) {
-            let mut actual_solution = self.mdp_solution.clone();
-            actual_solution.insert(point.clone());
-            if actual_solution.calculate_diversity() > max_diversity {
-              max_diversity = actual_solution.calculate_diversity();
-              selected = point.clone();
-            }
-          }
-        }
-        self.mdp_solution.insert(selected);
+    self.mdp_solution = MDPGrasp::new(self.mdp_solution.mdp_problem().clone(), self.size_m).execute().clone();
+    let mut best_distance = self.mdp_solution.calculate_diversity();
+    let mut current_solution = self.mdp_solution.clone();
+    let mut iterations = 0;
+    while iterations < self.max_iterations {
+      let candidate_solution = generate_candidate_solution(current_solution.clone());
+      let candidate_distance = candidate_solution.calculate_diversity();
+      if candidate_distance < best_distance {
+        self.mdp_solution = candidate_solution.clone();
+        best_distance = candidate_solution.calculate_diversity();
       }
-      self.tabu_list.push(self.mdp_solution.clone());
+      self.tabu_list.push_back(candidate_solution.clone());
+      if self.tabu_list.len() > self.size_m as usize {
+        self.tabu_list.pop_front();
+      }
+      current_solution = candidate_solution.clone();
+      iterations += 1;
     }
     return &self.mdp_solution;
   }
+}
+
+fn generate_candidate_solution(current_solution: MDPSolution) -> MDPSolution {
+  let mut env = Environment::new(current_solution.mdp_problem().clone(), current_solution.clone());
+  let neighbours: Vec<MDPSolution> = env.generate_neighbours();
+  let mut candidate_solution = current_solution.clone();
+  for neighbour in neighbours {
+    if neighbour.calculate_diversity() > candidate_solution.calculate_diversity() {
+      candidate_solution = neighbour;
+    }
+  }
+  candidate_solution
 }
